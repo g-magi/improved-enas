87af3da2da81270359eeb3ad309bc93c21a2c115
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-03-30 13:05:30 +0200

    cambiato da spazi a tab

--- common_ops.py
+++ common_ops.py
@@ -17,71 +17,71 @@ def lstm(x, prev_c, prev_h, w):
 def stack_lstm(x, prev_c, prev_h, w):
   next_c, next_h = [], []
   for layer_id, (_c, _h, _w) in enumerate(zip(prev_c, prev_h, w)): 
-    inputs = x if layer_id == 0 else next_h[-1]
-    curr_c, curr_h = lstm(inputs, _c, _h, _w)
-    next_c.append(curr_c)
-    next_h.append(curr_h)
+	inputs = x if layer_id == 0 else next_h[-1]
+	curr_c, curr_h = lstm(inputs, _c, _h, _w)
+	next_c.append(curr_c)
+	next_h.append(curr_h)
   return next_c, next_h
 
 
 def create_weight(name, shape, initializer=None, trainable=True, seed=None):
   if initializer is None:
-    initializer = tf.keras.initializers.he_normal(seed=seed)
+	initializer = tf.keras.initializers.he_normal(seed=seed)
   return tf.get_variable(name, shape, initializer=initializer, trainable=trainable)
 
 
 def create_bias(name, shape, initializer=None):
   if initializer is None:
-    initializer = tf.constant_initializer(0.0, dtype=tf.float32)
+	initializer = tf.constant_initializer(0.0, dtype=tf.float32)
   return tf.get_variable(name, shape, initializer=initializer)
 
 def batch_norm(x, is_training, name="bn", decay=0.9, epsilon=1e-5,
-               data_format="NHWC"):
+			   data_format="NHWC"):
   if data_format == "NHWC":
-    shape = [x.get_shape()[3]]
+	shape = [x.get_shape()[3]]
   elif data_format == "NCHW":
-    shape = [x.get_shape()[1]]
+	shape = [x.get_shape()[1]]
   else:
-    raise NotImplementedError("Unknown data_format {}".format(data_format))
+	raise NotImplementedError("Unknown data_format {}".format(data_format))
 
   with tf.variable_scope(name, reuse=None if is_training else True):
-    offset = tf.get_variable(
-      "offset", shape,
-      initializer=tf.constant_initializer(0.0, dtype=tf.float32))
-    scale = tf.get_variable(
-      "scale", shape,
-      initializer=tf.constant_initializer(1.0, dtype=tf.float32))
-    moving_mean = tf.get_variable(
-      "moving_mean", shape, trainable=False,
-      initializer=tf.constant_initializer(0.0, dtype=tf.float32))
-    moving_variance = tf.get_variable(
-      "moving_variance", shape, trainable=False,
-      initializer=tf.constant_initializer(1.0, dtype=tf.float32))
+	offset = tf.get_variable(
+	  "offset", shape,
+	  initializer=tf.constant_initializer(0.0, dtype=tf.float32))
+	scale = tf.get_variable(
+	  "scale", shape,
+	  initializer=tf.constant_initializer(1.0, dtype=tf.float32))
+	moving_mean = tf.get_variable(
+	  "moving_mean", shape, trainable=False,
+	  initializer=tf.constant_initializer(0.0, dtype=tf.float32))
+	moving_variance = tf.get_variable(
+	  "moving_variance", shape, trainable=False,
+	  initializer=tf.constant_initializer(1.0, dtype=tf.float32))
 
-    if is_training:
-      x, mean, variance = tf.nn.fused_batch_norm(
-        x, scale, offset, epsilon=epsilon, data_format=data_format,
-        is_training=True)
-      update_mean = moving_averages.assign_moving_average(
-        moving_mean, mean, decay)
-      update_variance = moving_averages.assign_moving_average(
-        moving_variance, variance, decay)
-      with tf.control_dependencies([update_mean, update_variance]):
-        x = tf.identity(x)
-    else:
-      x, _, _ = tf.nn.fused_batch_norm(x, scale, offset, mean=moving_mean,
-                                       variance=moving_variance,
-                                       epsilon=epsilon, data_format=data_format,
-                                       is_training=False)
+	if is_training:
+	  x, mean, variance = tf.nn.fused_batch_norm(
+		x, scale, offset, epsilon=epsilon, data_format=data_format,
+		is_training=True)
+	  update_mean = moving_averages.assign_moving_average(
+		moving_mean, mean, decay)
+	  update_variance = moving_averages.assign_moving_average(
+		moving_variance, variance, decay)
+	  with tf.control_dependencies([update_mean, update_variance]):
+		x = tf.identity(x)
+	else:
+	  x, _, _ = tf.nn.fused_batch_norm(x, scale, offset, mean=moving_mean,
+									   variance=moving_variance,
+									   epsilon=epsilon, data_format=data_format,
+									   is_training=False)
   return x
 
 def global_avg_pool(x, data_format="NHWC"):
   if data_format == "NHWC":
-    x = tf.reduce_mean(x, [1, 2])
+	x = tf.reduce_mean(x, [1, 2])
   elif data_format == "NCHW":
-    x = tf.reduce_mean(x, [2, 3])
+	x = tf.reduce_mean(x, [2, 3])
   else:
-    raise NotImplementedError("Unknown data_format {}".format(data_format))
+	raise NotImplementedError("Unknown data_format {}".format(data_format))
   return x
 
 def drop_path(x, keep_prob):
