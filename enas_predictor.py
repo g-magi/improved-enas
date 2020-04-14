b3f49046343e237ad27346f858b85788beb52cf1
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-04-14 17:59:28 +0200

    Update enas_predictor.py

--- enas_predictor.py
+++ enas_predictor.py
@@ -3,12 +3,12 @@ import pandas as pd
 import numpy as np
 
 
-def get_predictor(acc_seqs,final_accs)
+def get_predictor(acc_seqs,final_accs):
 	# TODO creare predittore e ritornarlo
 	predictor = SVR(kernel='rbf', C=1e4, gamma=0.1)
 	predictor.fit(acc_seqs, final_accs)
 	return predictor
     
-def get_prediction(acc_seq, predictor)
+def get_prediction(acc_seq, predictor):
     prediction = predictor.predict(acc_seq)
     return prediction
