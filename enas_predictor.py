67bbbaa2c3a5b441604fdd71aa2b2d632eb4e072
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-04-14 10:41:10 +0200

    Update enas_predictor.py

--- enas_predictor.py
+++ enas_predictor.py
@@ -1,18 +1,8 @@
 from sklearn.svm import SVR
 import pandas as pd
 from numpy import array
-from keras.models import Sequential
-from keras.layers import LSTM
-from keras.layers import Dense
 
 
-def get_prediction(data, partial_data, percentage=0.25):
-	prediction = 0.0
-	
-	n_steps = data[0].shape[0]
-	n_features = 1
-	model = Sequential()
-	model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
-	model.add(Dense(1))
-	
-	return prediction
+def train_predictor(X,y)
+    # TODO creare predittore e ritornarlo
+    
