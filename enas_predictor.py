715e6bef9e5b95d1cff1d029d1cda492412ac5b8
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-04-20 10:38:24 +0200

    inseriti diversi regressori con scelta

--- enas_predictor.py
+++ enas_predictor.py
@@ -1,11 +1,26 @@
 from sklearn.svm import SVR
+from sklearn.svm import LinearSVR
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import numpy as np
 
 
-def get_predictor(acc_seqs,final_accs):
+def get_predictor(acc_seqs,final_accs,predictor="linear"):
+	# data prep
 	acc_seqs = np.asarray(acc_seqs)
-	predictor = SVR(kernel='rbf', C=1e4, gamma=0.1)
+	if predictor is "linear":
+		predictor = linear_model.LinearRegression()
+	elif predictor is "svr-rbf":
+		predictor = SVR(kernel='rbf', C=1e4, gamma=0.1)
+	elif predictor is "svr-linear":
+		predictor = SVR(kernel='linear', C=1e4, gamma=0.1)
+	elif predictor is "linearSVR":
+		predictor = SVR(kernel='rbf', C=1e4, gamma=0.1)
+	elif predictor is "logistic":
+		predictor = LogisticRegression(solver='liblinear', random_state=0)
+	# type selection
+	
 	predictor.fit(acc_seqs, final_accs)
 	return predictor
 	
