8d2bac2a3e91c78fa3f7c306af1986bbc90c3282
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-05-06 11:06:17 +0200

    Update micro_controller.py

--- micro_controller.py
+++ micro_controller.py
@@ -76,8 +76,8 @@ class MicroController(Controller):
 		self.sample_entropy = entropy_1 + entropy_2
 		self.sample_log_prob = log_prob_1 + log_prob_2
 		
-		self.normal_array = tf.placeholder(tf.int32, shape=[None, 1],name="normal_array")
-		self.reduce_array = tf.placeholder(tf.int32, shape=[None, 1],name="reduce_array")
+		self.normal_array = tf.placeholder(tf.int32, shape=[None],name="normal_array")
+		self.reduce_array = tf.placeholder(tf.int32, shape=[None],name="reduce_array")
 		
 		
 		self.accuracy_scaling = accuracy_scaling
