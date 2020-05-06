0dde83d344f72674f67dcd763fbf8f1797c962f2
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-05-06 10:39:10 +0200

    Update micro_controller.py

--- micro_controller.py
+++ micro_controller.py
@@ -231,7 +231,7 @@ class MicroController(Controller):
 
 		return arc_seq, entropy, log_prob, last_c, last_h
 
-	def _set_train_dicts(normal_array, reduce_array):
+	def _set_train_dicts(self, normal_array, reduce_array):
 		return self.accuracy_scaling.convert_numpy_arrays_to_dicts(normal_array, reduce_array)
 	def build_trainer(self, child_model):
 		child_model.build_valid_rl()
@@ -243,7 +243,7 @@ class MicroController(Controller):
 		#self.reward = self.valid_acc 
 		
 		if self.normal_array is not None:
-			self.set_train_dicts = _set_train_dicts(self.normal_array, self.reduce_array)
+			self.set_train_dicts = self._set_train_dicts(self.normal_array, self.reduce_array)
 		
 		self.scaled_acc = self.accuracy_scaling.get_scaled_accuracy(
 									self.valid_acc,
