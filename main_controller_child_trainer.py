d95b971302adf5173447f755e35c793fe2e96968
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-05-06 18:31:32 +0200

    Update main_controller_child_trainer.py

--- main_controller_child_trainer.py
+++ main_controller_child_trainer.py
@@ -344,7 +344,7 @@ def train():
 								controller_ops["normal_dict_array"],
 								controller_ops["reduce_dict_array"],
 							]
-							loss, entropy, lr, gn, val_acc, normal_arc, reduce_arc, scaled_acc, bl, skip, _, normal_dict_array, reduce_dict_array = sess.run(run_ops,feed_dict={"normal_array:0":temp_normal_array, "reduce_array:0":temp_reduce_array})
+							loss, entropy, lr, gn, val_acc, normal_arc, reduce_arc, scaled_acc, bl, skip, _, normal_dict_array, reduce_dict_array = sess.run(run_ops,feed_dict={"normal_array:0":tf.convert_to_tensor(temp_normal_array), "reduce_array:0":tf.convert_to_tensor(temp_reduce_array)})
 							print("running controller step")
 							controller_step = sess.run(controller_ops["train_step"])
 							
