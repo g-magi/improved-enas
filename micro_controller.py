b22b63007c8bd26d085a1fdfdbb1ec666dda31aa
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-05-12 13:39:53 +0200

    Update micro_controller.py

--- micro_controller.py
+++ micro_controller.py
@@ -250,8 +250,8 @@ class MicroController(Controller):
 									self.valid_acc,
 									self.current_normal_arc,
 									self.current_reduce_arc,
-									scaling_method=tf.constant("linear"),
-									arc_handling=tf.constant("sum"))
+									scaling_method=tf.constant("average"),
+									arc_handling=tf.constant("avg"))
 		self.reward = self.scaled_acc
 		
 		if self.entropy_weight is not None:
