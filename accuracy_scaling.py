845d343584d87a35b9caeb1d2845099fb32396cf
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-05-12 15:13:55 +0200

    Update accuracy_scaling.py

--- accuracy_scaling.py
+++ accuracy_scaling.py
@@ -258,4 +258,4 @@ class AccuracyScaler:
 		
 		scaling_factor = scaled_accuracy/accuracy
 		
-		return scaled_accuracy, tf_normal_arc_training, tf_normal_dict.export()[1]
+		return scaled_accuracy, tf_normal_arc_seq, tf_normal_dict.export()[1]
