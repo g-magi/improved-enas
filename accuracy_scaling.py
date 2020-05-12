426baac9fae102de16f34bfe21f837fffaee616c
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-05-12 12:03:52 +0200

    Update accuracy_scaling.py

--- accuracy_scaling.py
+++ accuracy_scaling.py
@@ -286,4 +286,4 @@ class AccuracyScaler:
 		scaled_accuracy = tf.cond(tf.equal(scaling_method, tf.constant("linear")),_scale_linear,_scale_avg)
 		
 		
-		return scaled_accuracy, tf_normal_arc_training_sum, tf_reduce_arc_training_sum
+		return scaled_accuracy, tf_normal_arc_training, tf_reduce_arc_training
