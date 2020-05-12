9e42d0fdce1377d6db868d59a3d4278dd7c19849
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-05-12 13:34:11 +0200

    Update accuracy_scaling.py

--- accuracy_scaling.py
+++ accuracy_scaling.py
@@ -188,7 +188,7 @@ class AccuracyScaler:
 			return i, seq, arc
 		
 		seq = tf.while_loop(_cond, _body, loop_tuple)[1]
-		seq = tf.reshape(seq,[-1])
+		seq = tf.reshape(seq.stack(),[-1])
 		return seq
 		
 	"""
