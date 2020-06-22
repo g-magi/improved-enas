eb41ccb9efc23efad6f0417774061a798ce716ed
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-06-22 17:37:34 +0200

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -313,6 +313,7 @@ def _parents_read_images(img_size=64, channels=3):
 			parent_img_number = str(j).zfill(3)
 			parent_img_filename = data["prefixes"][i]+"_"+parent_img_number+"_1.jpg"
 			parent_img_path += os.sep+parent_img_filename
+			print("\tReading image: ",parent_img_path)
 			parent_img = cv2.imread(parent_img_path)
 			parent_img = cv2.resize(parent_img,(img_size,img_size))
 			parent_img = np.reshape(parent_img, [1,img_size,img_size,channels])
