b1d2029378248b6dbad5a51a33c6c6beb8ed367f
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-06-26 16:03:14 +0200

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -363,7 +363,7 @@ def _parents_read_images(img_size=64, channels=3):
 			parent_img = cv2.imread(parent_img_path)
 			#parent_img = cv2.cvtColor(parent_img, cv2.COLOR_BGR2GRAY)
 			parent_img = cv2.resize(parent_img,(img_size,img_size))
-			parent_img = np.reshape(parent_img, [img_size,img_size,1])
+			parent_img = np.reshape(parent_img, [img_size,img_size,channels])
 			
 			child_img_path = data["paths"][i]
 			child_img_number = str(j+1).zfill(3)
@@ -372,7 +372,7 @@ def _parents_read_images(img_size=64, channels=3):
 			child_img = cv2.imread(child_img_path)
 			#child_img = cv2.cvtColor(child_img, cv2.COLOR_BGR2GRAY)
 			child_img = cv2.resize(child_img,(img_size,img_size))
-			child_img = np.reshape(child_img, [img_size,img_size,1])
+			child_img = np.reshape(child_img, [img_size,img_size,channels])
 			
 			data["pairs"][i]["parent"].append(parent_img)
 			data["pairs"][i]["child"].append(child_img)
