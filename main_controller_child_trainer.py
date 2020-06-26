83648d1ca2ce8ac110e79373865dff64e7bd1b4e
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-06-26 16:07:23 +0200

    Update main_controller_child_trainer.py

--- main_controller_child_trainer.py
+++ main_controller_child_trainer.py
@@ -35,7 +35,7 @@ DEFINE_string("train_data_dir", "./data/train", "")
 DEFINE_string("val_data_dir", "./data/valid", "")
 DEFINE_string("test_data_dir", "./data/test", "")
 DEFINE_integer("channel",6, "MNIST/fashion_MNIST: 1, Cifar10/Cifar100: 3, parents: 3, parents_img: 2")
-DEFINE_integer("img_size", 32, "enlarge image size")
+DEFINE_integer("img_size", 64, "enlarge image size")
 DEFINE_integer("n_aug_img",1 , "if 2: num_img: 55000 -> aug_img: 110000, elif 1: False")
 DEFINE_string("data_source","parents_img","either 'parents', 'mnist', 'cifar10', 'cifar100' 'parents_img', 'fashion_mnist' ")
 ##########################################################################
