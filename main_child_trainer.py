7790086ef72080c8d93a7e2603429e9f92fa8614
Gabriele Magistrali <gmagistrali@hotmail.it>
2020-07-15 10:14:54 +0200

    switched to fashion_mnist for local tensorboard test

--- main_child_trainer.py
+++ main_child_trainer.py
@@ -37,10 +37,10 @@ DEFINE_string("best_arcs_filename","best_arcs.csv","")
 DEFINE_string("train_data_dir", "./data/train", "")
 DEFINE_string("val_data_dir", "./data/valid", "")
 DEFINE_string("test_data_dir", "./data/test", "")
-DEFINE_integer("channel",3, "MNIST/fashion_MNIST: 1, Cifar10/Cifar100: 3, parents: 3, parents_img: 6")
+DEFINE_integer("channel",1, "MNIST/fashion_MNIST: 1, Cifar10/Cifar100: 3, parents: 3, parents_img: 6")
 DEFINE_integer("img_size", 32, "enlarge image size")
 DEFINE_integer("n_aug_img",1 , "if 2: num_img: 55000 -> aug_img: 110000, elif 1: False")
-DEFINE_string("data_source","cifar10","either 'parents', 'mnist', 'cifar10', 'cifar100' 'parents_img', 'fashion_mnist' ")
+DEFINE_string("data_source","fashion_mnist","either 'parents', 'mnist', 'cifar10', 'cifar100' 'parents_img', 'fashion_mnist' ")
 ##########################################################################
 
 DEFINE_boolean("reset_output_dir", True, "Delete output_dir if exists.")
@@ -300,7 +300,7 @@ def train():
 		checkpoint_saver_hook = tf.train.CheckpointSaverHook(
 			FLAGS.output_dir, save_steps=child_ops["num_train_batches"], saver=saver)
 		hooks = []
-		#hooks = [checkpoint_saver_hook]
+		hooks = [checkpoint_saver_hook]
 		if FLAGS.child_sync_replicas:
 			sync_replicas_hook = child_ops["optimizer"].make_session_run_hook(True)
 			hooks.append(sync_replicas_hook)
